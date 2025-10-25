61ad07e65a43c787f1c3152e54304171b41f80dc
Kata <katachanneloffical@gmail.com>
2025-10-25 13:26:29 +0700

    Auto commit at 2025-10-25 13:26:29

--- scripts/3deploy.sh
+++ scripts/3deploy.sh
@@ -7,7 +7,7 @@ git push
 
 # Remote server operations
 ssh root@116.118.49.243 << 'EOF'
-cd rausachfinal
+cd shoprausach
 git pull
 docker compose -f 'docker-compose.yml' up -d --build
 docker builder prune -af
