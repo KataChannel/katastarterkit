c031906cfd66c5c36884cc82b88425061f4dadbd
Kata <katachanneloffical@gmail.com>
2025-10-26 20:24:28 +0700

    update

--- frontend/src/components/admin/users/index.ts
+++ frontend/src/components/admin/users/index.ts
@@ -7,8 +7,6 @@
 // Main components
 export { UserManagementHeader } from './UserManagementHeader';
 export { UserManagementContent } from './UserManagementContent';
-export { UserActionBar } from './UserActionBar';
-export { UserSearchBar } from './UserSearchBar';
 
 // State components
 export { LoadingState } from './LoadingState';
@@ -17,7 +15,6 @@ export { AccessDenied } from './AccessDenied';
 
 // Feature components (already exported elsewhere)
 export { UserStats } from './UserStats';
-export { UserFilters } from './UserFilters';
 export { UserTable } from './UserTable';
 export { UserTableSkeleton } from './UserTableSkeleton';
 export { TablePagination } from './TablePagination';
