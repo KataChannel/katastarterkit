8dfb538de702063385faccc5a75a4abbc8ef1a82
Chí Kiệt <chikiet88@gmail.com>
2025-10-30 16:52:34 +0700

    Auto commit at 2025-10-30 16:52:34

--- backend/src/lms/certificates/certificates.module.ts
+++ backend/src/lms/certificates/certificates.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '../../prisma/prisma.module';
 import { CertificatesService } from './certificates.service';
 import { CertificatesResolver } from './certificates.resolver';
-import { PrismaModule } from '../../prisma/prisma.module';
 import { AuthModule } from '../../auth/auth.module';
+import { UserService } from '../../services/user.service';
 
 @Module({
   imports: [PrismaModule, AuthModule],
-  providers: [CertificatesService, CertificatesResolver],
+  providers: [CertificatesService, CertificatesResolver, UserService],
   exports: [CertificatesService],
 })
 export class CertificatesModule {}
--- backend/src/lms/discussions/discussions.module.ts
+++ backend/src/lms/discussions/discussions.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '../../prisma/prisma.module';
 import { DiscussionsService } from './discussions.service';
 import { DiscussionsResolver } from './discussions.resolver';
-import { PrismaModule } from '../../prisma/prisma.module';
 import { AuthModule } from '../../auth/auth.module';
+import { UserService } from '../../services/user.service';
 
 @Module({
   imports: [PrismaModule, AuthModule],
-  providers: [DiscussionsService, DiscussionsResolver],
+  providers: [DiscussionsService, DiscussionsResolver, UserService],
   exports: [DiscussionsService],
 })
 export class DiscussionsModule {}
