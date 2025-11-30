410e88b6199b5040cb7c9975bbecb27ef7e73773
Kata <katachanneloffical@gmail.com>
2025-11-30 11:59:56 +0700

    Auto commit at 2025-11-30 11:59:56

--- docs/31-frontend__next-internal_server_app_demo_login_page_actions_dad5ae81.js
+++
@@ -1,3 +0,0 @@
-module.exports=[460745,(a,b,c)=>{}];
-
-//# sourceMappingURL=frontend__next-internal_server_app_demo_login_page_actions_dad5ae81.js.map
--- docs/40-frontend__next-internal_server_app_demo_chatbot_page_actions_e6f793bf.js
+++
@@ -1,3 +0,0 @@
-module.exports=[792488,(a,b,c)=>{}];
-
-//# sourceMappingURL=frontend__next-internal_server_app_demo_chatbot_page_actions_e6f793bf.js.map
